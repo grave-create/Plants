10f7f6d36ffc9462422688becc20df7f3d9b4510
woshicaidog <73059545+woshicaidog@users.noreply.github.com>
2021-12-27 23:54:33 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QCoreApplication>
+#include <QtWidgets>
+#include <QQmlContext>
+#include <QtMultimedia>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp> //引用cv::KeyPoint 特征检测器通用接口
+#include <opencv2/calib3d/calib3d.hpp>
+#include  <iostream>
+#include <iomanip>
+#include <fstream>
+#include"CameraCalibrator.h"
+using namespace std;
+using namespace cv;
+int main(int argc, char *argv[])
+{
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    //qmlRegisterType<CameraCalibrator>("Sources.CameraCalibrator", 1, 0, "CAMERA");
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    CameraCalibrator cameracalibrator;
+    engine.rootContext()->setContextProperty("CameraCalibrator", &cameracalibrator);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+    return app.exec();
+}
